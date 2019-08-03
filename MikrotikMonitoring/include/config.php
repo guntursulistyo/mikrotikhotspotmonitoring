86ef1b7f7a36e145b91011c4526645c702d177eb
PT. Netzen Media Akses <12862916+netzenmediaakses@users.noreply.github.com>
2019-08-03 18:05:13 +0800

    Update config.php

--- MikrotikMonitoring/include/config.php
+++ MikrotikMonitoring/include/config.php
@@ -1,9 +1,3 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<guntur','mikhmon>|>epqcoaGenaWTo2lq');
-
-$data['Monica-Hotspot'] = array ('1'=>'Monica-Hotspot!10.15.15.20','Monica-Hotspot@|@netzen','Monica-Hotspot#|#ppamspagcWo=','Monica-Hotspot%monica.net','Monica-Hotspot^monica.net','Monica-Hotspot&Rp','Monica-Hotspot*10','Monica-Hotspot(1','Monica-Hotspot)0','Monica-Hotspot=0','Monica-Hotspot@!@enable');
-$data['Hotspot-Selfi'] = array ('1'=>'Hotspot-Selfi!10.15.15.16','Hotspot-Selfi@|@netzen','Hotspot-Selfi#|#ppamspagcWo=','Hotspot-Selfi%alfimidi.net','Hotspot-Selfi^alfimidi.net','Hotspot-Selfi&Rp','Hotspot-Selfi*10','Hotspot-Selfi(1','Hotspot-Selfi)0','Hotspot-Selfi=0','Hotspot-Selfi@!@disable');
-$data['Hotspot-Rido'] = array ('1'=>'Hotspot-Rido!10.15.15.9','Hotspot-Rido@|@netzen','Hotspot-Rido#|#ppamspagcWo=','Hotspot-Rido%rido.net','Hotspot-Rido^rido.net','Hotspot-Rido&Rp','Hotspot-Rido*10','Hotspot-Rido(1','Hotspot-Rido)0','Hotspot-Rido=0','Hotspot-Rido@!@disable');
-$data['Hotspot-Lilik'] = array ('1'=>'Hotspot-Lilik!10.15.15.14','Hotspot-Lilik@|@netzen','Hotspot-Lilik#|#ppamspagcWo=','Hotspot-Lilik%lilik.net','Hotspot-Lilik^lilik.net','Hotspot-Lilik&Rp','Hotspot-Lilik*10','Hotspot-Lilik(1','Hotspot-Lilik)0','Hotspot-Lilik=0','Hotspot-Lilik@!@disable');
-$data['HotspotLaluDeni'] = array ('1'=>'HotspotLaluDeni!10.15.15.63','HotspotLaluDeni@|@netzen','HotspotLaluDeni#|#ppamspagcWo=','HotspotLaluDeni%hotspotLaluDeni','HotspotLaluDeni^annisa.hotspot','HotspotLaluDeni&Rp','HotspotLaluDeni*10','HotspotLaluDeni(1','HotspotLaluDeni)0','HotspotLaluDeni=0','HotspotLaluDeni@!@disable');
